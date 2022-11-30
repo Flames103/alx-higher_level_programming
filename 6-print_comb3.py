5a445d75d8b0c3a86a78005d776329ff5978df5d
flames103 <beeflamez1@gmail.com>
2022-11-29 17:00:05 -0800

    happiness loves me

---
+++ 6-print_comb3.py
@@ -0,0 +1,10 @@
+Executable File  8 lines (7 sloc)  231 Bytes
+
+#!/usr/bin/python3
+
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print(f"{digit1}{digit2}")
+        else:
+            print(f"{digit1}{digit2}", end=", ")
