ff7cc966a5dda5b7810591f92f3fbe238531414d
flames103 <beeflamez1@gmail.com>
2022-12-01 16:09:41 -0800

    flames of eternity

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
 import hidden_4
+
+
+def main():
+    file = dir(hidden_4)
+    length = len(file)
+    for i in range(0, length):
+        if file[i][0:2] != "__":
+            print(file[i])
+
+
 if __name__ == "__main__":
-    for i in dir(hidden_4):
-        if i[0:2] != "__":
-            print("{:s}".format(i))
+    main()
