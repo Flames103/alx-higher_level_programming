8d193963eac08480d22a61440c358c110a5202f7
flames103 <beeflamez1@gmail.com>
2023-01-10 17:08:51 -0800

    goal goal

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,20 +1,3 @@
-
-Torivicky
-/
-alx-higher_level_programming
-Public
-Code
-Issues
-Pull requests
-Actions
-Projects
-Security
-Insights
-alx-higher_level_programming/0x0B-python-input_output/7-add_item.py
-@Torivicky
-Torivicky Python Input and Output
- 1 contributor
-Executable File  15 lines (13 sloc)  481 Bytes
 #!/usr/bin/python3
 """Add all arguments to a Python list and save them to a file."""
 import sys
